821a55d76df33fb5dd4c6c20b82ea961df1e6c72
Maxima Max <70286446+aditya-the-pro@users.noreply.github.com>
2020-12-21 14:20:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -5,10 +5,9 @@ ImgInsta is easiest and most convenient way to download HD profile photos from I
 
 **For Anti-Phishing Behaviour Of Browsers For Downloading Multiple Files At Once Allow Via Pop-Up**
 
-TO-DO:
+TO-DO:<br />
 1.If a user enters null/blank value in textarea should i need to alert it also? <br />
 2.improve the UI more sometimes font don't loaad on firefox android properly (so i'll be adding fallback font). <br />
 3.Adding a msg when javascript is disabled <br />
 4.may be i add reelinsta or vidinsta (download reels and videos also) <br />
-![alt there is a image](https://github.com/aditya-the-pro/ImgInsta/blob/master/insta.jpg?raw=true)
 Credit:-@abhishek-g0yal
---
+++ assests/js/insta.js
@@ -0,0 +1,30 @@
+async function downloadBlobs(userName) {
+    function downloadBlob(blob, filename) {
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename || 'download';
+        a.click();
+    }
+    userName = userName.split(" ");
+    for(let i = 0; i < userName.length; i++) {
+        console.log(userName[i])
+        let instaURL = 'https://www.instagram.com/'+userName[i]+'/?__a=1'
+        console.log(instaURL)
+        let instaFetch = await fetch(instaURL)
+        if(instaFetch.ok) {
+            let instaJSON = await instaFetch.json()
+            let profilePicURL = instaJSON.graphql.user.profile_pic_url_hd
+            console.log(profilePicURL)
+            let profilePicFetch = await fetch(profilePicURL)
+            if(profilePicFetch.ok) {
+                let profilePicBlob = await profilePicFetch.blob()
+                downloadBlob(profilePicBlob, userName[i] + '.jpg')
+            }
+        }
+        else {
+            console.log("HTTP-Error: " + instaFetch.status)
+            alert('You entered wrong or @ with username');
+        }
+    }
+}
