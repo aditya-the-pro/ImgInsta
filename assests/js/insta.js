95bc7fdd2bedf06af335a685d00e809c69979788
Maxima Max <70286446+aditya-the-pro@users.noreply.github.com>
2021-04-01 20:29:42 +0530

    Update insta.js

--- assests/js/insta.js
+++ assests/js/insta.js
@@ -13,7 +13,9 @@ async function downloadBlobs(userName) {
         console.log(userName[i])
         let instaURL = 'https://www.instagram.com/'+userName[i]+'/?__a=1'
         console.log(instaURL)
-        let instaFetch = await fetch(instaURL,{mode: 'no-cors'})
+        let instaFetch = await fetch(instaURL,{headers: {
+            Access-Control-Allow-Origin: *
+  }})
         if(instaFetch.ok) {
             let instaJSON = await instaFetch.json()
             let profilePicURL = instaJSON.graphql.user.profile_pic_url_hd
